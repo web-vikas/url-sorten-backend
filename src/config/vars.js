c9b36a5af34aa2f203b227498caa9e6ca3750d00
web-vikas <msg_vikas@yahoo.com>
2024-01-24 19:08:22 +0530

    Removed Variable CHeck

--- src/config/vars.js
+++ src/config/vars.js
@@ -4,13 +4,13 @@ const dotenvExample = require("dotenv").config({
   path: path.resolve(process.cwd(), ".env.example"),
 });
 
-if (
-  Object !== null &&
-  JSON.stringify(Object.keys(dotenv.parsed).sort()) !==
-    JSON.stringify(Object.keys(dotenvExample.parsed).sort())
-) {
-  throw Error("Missing values in .env. Please refer to .env.example");
-}
+// if (
+//   Object !== null &&
+//   JSON.stringify(Object.keys(dotenv.parsed).sort()) !==
+//     JSON.stringify(Object.keys(dotenvExample.parsed).sort())
+// ) {
+//   throw Error("Missing values in .env. Please refer to .env.example");
+// }
 
 module.exports = {
   port: process.env.PORT,
